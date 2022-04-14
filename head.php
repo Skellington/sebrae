9d62a9835243b561db5c11c2f54ec05992747932
Raphael Gomes <raphaelgs@outlook.com>
2022-04-14 01:31:02 -0300

    Add files via upload

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<link href="./assets/css/style.css" rel="stylesheet">	
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="icon" type="image/x-icon" href="./assets/images/favicon.png">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>	
+	<script src='./assets/js/fade.js'></script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<script src='./assets/js/menu.js'></script>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include'head.php';?>
+<title>Home</title>
+<?php include'header.php';?>
+	<main id="pt" class="home E-fadein" tabindex="-1" role="main">
+		<section class="sustentabilidade1">
+			<article class="container">
+				<span class="elementos-azul1">
+					<svg id="Grupo_550" data-name="Grupo 550" xmlns="http://www.w3.org/2000/svg" width="449.576" height="169.04" viewBox="0 0 449.576 169.04">
+						<g id="Grupo_37" data-name="Grupo 37">
+							<path id="Caminho_24" data-name="Caminho 24" d="M376.062,0l36.757,169.02L.385,169.04-36.757,0Z" transform="translate(36.757)" fill="#003fd8"/>
+						</g>
+					</svg>
+				</span>
+				<span class="elementos-branco">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1407.965" height="837.859" viewBox="0 0 1407.965 837.859">
+
+					    <g id="Grupo_548" data-name="Grupo 548" transform="translate(-256.138 -71.24)">
+					      <g id="Grupo_31" data-name="Grupo 31" transform="translate(0)">
+					        <path id="Caminho_21" data-name="Caminho 21" d="M609.381,0,669.69,274.479H-.626L-60.309.037v0Z" transform="translate(60.309)" fill="#fff"/>
+					      </g>
+					    </g>
+					  </g>
+					</svg>
+				</span>
+				<div class="titulo">
+					<h2>Relatório de<br>Sustentabilidade<br><span>2021</span></h2>
+					<a class="botao" href=""><span>SUMÁRIO EXECUTIVO</span></a>
+				</div>
+				<div class="imagem">
+					<img src="./assets/images/imagem-susten.png" alt="Sebrae Minas Gerais">
+				</div>
+				<span class="elementos-azul2">
+					<svg xmlns="http://www.w3.org/2000/svg" width="607.771" height="69.868" viewBox="0 0 607.771 69.868">
+					  <g id="Grupo_549" data-name="Grupo 549" transform="translate(0)">
+					    <g id="Grupo_33" data-name="Grupo 33" transform="translate(0 0)">
+					      <path id="Caminho_22" data-name="Caminho 22" d="M569.039-.018l22.2,69.868H.352L-16.537-.018Z" transform="translate(16.537 0.018)" fill="#003fd8"/>
+					    </g>
+					  </g>
+					</svg>
+				</span>
+			</article>
+		</section>
+		<section class="sustentabilidade">
+			<img src="./assets/images/fundo-sustenta.png">
+			<article class="container">				
+				<div class="titulo">
+					<h2>Relatório de<br>Sustentabilidade<br><span>2021</span></h2>
+					<a class="botao" href=""><span>PDF DO RELATÓRIO 2021</span></a>
+				</div>		
+			</article>
+		</section>
+	</main>
+
+	<?php include'footer.php';?>
+
+
